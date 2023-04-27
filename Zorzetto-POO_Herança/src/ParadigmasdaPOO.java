0f90a7f3ef24439ad49a83307a3df3c00da8583a
Gizetto61 <127257041+Gizetto61@users.noreply.github.com>
2023-04-27 09:19:07 -0300

    Add files via upload

---
+++ Zorzetto-POO_Herança/src/ParadigmasdaPOO.java
@@ -0,0 +1,13 @@
+public class ParadigmasdaPOO
+{
+    public static void main(String[] args)
+    {
+        Pessoa pessoa1 = new Pessoa( "Giovanni", "11/05", 16);
+        pessoa1.apresentar();
+
+
+
+        Pessoal pergunta = new Pessoal();
+        pergunta.perguntar();
+    }
+}
